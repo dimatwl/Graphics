dc0f154dcc776937550f3ba476f2ea79b034dcf4
Dmitriy Shestavin <dimatwl@gmail.com>
2012-12-15 13:32:08 +0400

    Controls improvements.

--- js/OrbitControls.js
+++ js/OrbitControls.js
@@ -304,7 +304,7 @@ THREE.OrbitControls = function ( object, domElement ) {
 
 	}
 
-	this.domElement.addEventListener( 'contextmenu', function ( event ) { event.preventDefault(); }, false );
+	//this.domElement.addEventListener( 'contextmenu', function ( event ) { event.preventDefault(); }, false );
 	this.domElement.addEventListener( 'mousedown', onMouseDown, false );
 	this.domElement.addEventListener( 'mousewheel', onMouseWheel, false );
 	this.domElement.addEventListener( 'keypress', onKeyDown, false );
